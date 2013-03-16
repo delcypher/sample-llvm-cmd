066197e9efd727a8570afa1ca50e2584df82c736
Dan Liew <delcypher@gmail.com>
2013-03-16 22:09:02 +0000

    Cleaned up AutoRegen.sh - There was some absolutely pointless code in there. It collect varibles llvm_src_root and llvm_obj_root variables but then did nothing with them!!
    Regenerated configure just to test the AutoRegen.sh script.

--- autoconf/AutoRegen.sh
+++ autoconf/AutoRegen.sh
@@ -10,32 +10,6 @@ if test $? -ne 0 ; then
   die "Your autoconf was not detected as being 2.5x or 2.6x"
 fi
 cwd=`pwd`
-if test -d ../../../autoconf/m4 ; then
-  cd ../../../autoconf/m4
-  llvm_src_root=../..
-  llvm_obj_root=../..
-  cd $cwd
-elif test -d ../../llvm/autoconf/m4 ; then
-  cd ../../llvm/autoconf/m4
-  llvm_src_root=../..
-  llvm_obj_root=../..
-  cd $cwd
-else
-  while true ; do
-    echo "LLVM source root not found." 
-    read -p "Enter full path to LLVM source:" REPLY
-    if test -d "$REPLY/autoconf/m4" ; then
-      llvm_src_root="$REPLY"
-      read -p "Enter full path to LLVM objects (empty for same as source):" REPLY
-      if test -d "$REPLY" ; then
-        llvm_obj_root="$REPLY"
-      else
-        llvm_obj_root="$llvm_src_root"
-      fi
-      break
-    fi
-  done
-fi
 echo "Regenerating aclocal.m4 with aclocal"
 rm -f aclocal.m4
 aclocal -I $cwd/m4 || die "aclocal failed"
